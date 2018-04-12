eb676df04a3ae7a6ffa2625356e87325c98cb98a
draftLoca <1446617386@qq.com>
2018-04-12 18:08:07 +0800

    idesecond change

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,6 +4,6 @@
 public class HelloWorld {
     public static void main(String args[]){
         System.out.println("hello world");
-        System.out.println("good night world");
+        System.out.println("good night worl2d");
     }
 }
