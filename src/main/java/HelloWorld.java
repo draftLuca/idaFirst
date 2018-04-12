cf8aa88e28cc12c8222e3e2d59d6b6065192e8e8
draftLoca <1446617386@qq.com>
2018-04-12 20:21:18 +0800

    12

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,6 +6,6 @@ public class HelloWorld {
         System.out.println("hello world");
         System.out.println("good night by idafirs5t");
         System.out.println("good night worl2d");
-        System.out.println("change 12");
+        System.out.println("change 143452");
     }
 }
